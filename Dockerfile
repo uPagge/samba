967d5534e82ea8543ea15325a2a07e262ed7ddbf
Struchkov Mark <mark@struchkov.dev>
2023-03-05 13:17:53 +0300

    Удалил лишние файлы, актуализировал имя владельца

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine
-MAINTAINER David Personette <dperson@gmail.com>
+MAINTAINER Struchkov Mark <mark@struchkov.dev>
 
 # Install samba
 RUN apk --no-cache --no-progress upgrade && \
--- samba.sh
+++ samba.sh
@@ -10,7 +10,7 @@
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: David Personette (dperson@gmail.com),
+#        AUTHOR: Struchkov Mark (mark@struchkov.dev),
 #  ORGANIZATION:
 #       CREATED: 09/28/2014 12:11
 #      REVISION: 1.0
